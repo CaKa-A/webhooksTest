33eab6aeafe4d27cf730ffb20ac69f4b6e74accb
zhengzhong <1945261355@qq.com>
2022-07-11 15:23:59 +0800

    feat: first commit

---
+++ src/main/java/com/example/webhooksdemo/WebhooksDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.webhooksdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhooksDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebhooksDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webhooksdemo/WebhooksDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webhooksdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhooksDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
